193ff2dc2401ac5c331c9cad1efa0e8497d41caf
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-12-05 20:33:18 -0300

    ft_strcpy

---
+++ 1/ft_strcpy.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 19:34:05 by peda-cos          #+#    #+#             */
+/*   Updated: 2024/12/05 19:37:31 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcpy(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s2[i])
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	s1[i] = '\0';
+	return (s1);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char *src = "aoba";
+	char dest[4];
+
+	ft_strcpy(dest, src);
+	printf("%s\n", dest);
+	return (0);
+}
