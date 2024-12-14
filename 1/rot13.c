03a0ecad65aac7839b4a1b1f670dd258367736e6
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-12-14 18:36:52 -0300

    rot13

--- 1/rot13.c
+++ 1/rot13.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/05 23:19:21 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/12/05 23:23:06 by peda-cos         ###   ########.fr       */
+/*   Created: 2024/12/14 17:08:41 by peda-cos          #+#    #+#             */
+/*   Updated: 2024/12/14 18:34:48 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,25 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+void	ft_rot(char *str)
+{
+	int	i = 0;
+
+	while (str[i])
+	{
+		if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
+			str[i] += 13;
+		else if ((str[i] >= 'n' && str[i] <= 'z') || (str[i] >= 'N' && str[i] <= 'Z'))
+			str[i] -= 13;
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
 int main(int argc, char **argv)
 {
-	int i = 0;
 	if (argc == 2)
-	{
-		while (argv[1][i])
-		{
-			if ((argv[1][i] >= 'A' && argv[1][i] <= 'M') || (argv[1][i] >= 'a' && argv[1][i] <= 'm'))
-				argv[1][i] += 13;
-			else if ((argv[1][i] >= 'N' && argv[1][i] <= 'Z') || (argv[1][i] >= 'n' && argv[1][i] <= 'z'))
-				argv[1][i] -= 13;
-			ft_putchar(argv[1][i]);
-			i++;
-		}
-	}
+		ft_rot(argv[1]);
 	ft_putchar('\n');
 	return (0);
 }
