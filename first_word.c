0255b0a6dcd6e3938e11edffb3d89f63551ae9e8
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-01-05 20:50:32 -0300

    first_word.c

--- first_word.c
+++ first_word.c
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 20:44:22 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/01/05 20:47:07 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/01/05 20:50:16 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,10 @@ int	main(int argc, char **argv)
 		while (argv[1][i] == ' ' || argv[1][i] == '\t')
 			i++;
 		while (argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
+		{
+			write(1, &argv[1][i], 1);
 			i++;
-		write(1, argv[1], i);
+		}
 	}
 	write(1, "\n", 1);
 	return (0);
