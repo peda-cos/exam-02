436a59909ded96a87b68205f8229c4818e692f01
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-02-02 06:54:50 -0300

    fix: update ulstr conversion arithmetic and header timestamp

--- Level 01/ulstr.c
+++ Level 01/ulstr.c
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 06:49:37 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/02/02 06:53:32 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/02/02 06:54:16 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ static int	ft_islower(char c)
 static void	ulstr(char c)
 {
 	if (ft_isupper(c))
-		c + 32;
+		c += 32;
 	else if (ft_islower(c))
-		c - 32;
+		c -= 32;
 	ft_putchar(c);
 }
 
